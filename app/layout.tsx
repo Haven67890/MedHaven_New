99ab18247f1016a8ce76fb0ed7f237d7cbf57e52
Haven67890 <dachenaugustine@gmail.com>
2026-07-31 12:59:04 +0100

    fix(providers): import ThemeProvider as named export in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
-import ThemeProvider from "@/components/providers/theme-provider"
+-import ThemeProvider from "@/components/providers/theme-provider"
++import { ThemeProvider } from "@/components/providers/theme-provider"
 import { AuthProvider } from '@/components/providers/AuthProvider'
 
 export const metadata = {
